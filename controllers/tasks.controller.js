f0a89c68c4891db5f7ca795a32f7a98407f52be0
Andrey <sudarev.andrey.i@gmail.com>
2022-01-08 13:04:23 +0300

    upd tasks

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,9 +1,17 @@
+const getDateFormat = require('../utils');
 const sql = require('mssql');
 const config = require('../config');
 
 class TasksController {
   async createTask(req, res) {
-    const { usergroup, TASK_NAME, PERIODICITY, DEADLINE, TASK_GROUP, TASK_DESCRIPTION } = req.query;
+    const {
+      usergroupid,
+      TASK_NAME,
+      PERIODICITY,
+      DEADLINE,
+      TASK_GROUP,
+      TASK_DESCRIPTION,
+    } = req.query;
     const pool = await sql.connect(config);
     const newTask = await pool.request().query(
       `INSERT INTO TASKS (
@@ -17,7 +25,7 @@ class TasksController {
         ) 
         OUTPUT inserted.*
         VALUES (
-        ${usergroup},
+        ${usergroupid},
         '${TASK_NAME}',
         ${PERIODICITY},
         ${DEADLINE},
@@ -30,29 +38,33 @@ class TasksController {
   }
 
   async getTasks(req, res) {
-    const { usergroup } = req.params;
+    const { usergroupid } = req.params;
     const pool = await sql.connect(config);
     const tasks = await pool
       .request()
-      .query(`SELECT * FROM TASKS WHERE USERGROUP_ID = ${usergroup};`);
+      .query(`SELECT * FROM TASKS WHERE USERGROUP_ID = ${usergroupid};`);
     res.json(tasks.recordset);
   }
 
   async countTasks(req, res) {
-    const { usergroup } = req.params;
+    const { usergroupid } = req.params;
     const pool = await sql.connect(config);
     const tasks = await pool
       .request()
-      .query(`SELECT COUNT(ID) AS COUNT_TASKS FROM TASKS WHERE USERGROUP_ID = ${usergroup};`);
+      .query(
+        `SELECT COUNT(ID) AS COUNT_TASKS FROM TASKS WHERE USERGROUP_ID = ${usergroupid};`
+      );
     res.json(tasks.recordset);
   }
 
   async getOneTask(req, res) {
-    const { taskid, usergroup } = req.params;
+    const { taskid, usergroupid } = req.params;
     const pool = await sql.connect(config);
     const user = await pool
       .request()
-      .query(`SELECT * FROM TASKS WHERE ID = ${taskid} AND USERGROUP_ID = ${usergroup};`);
+      .query(
+        `SELECT * FROM TASKS WHERE ID = ${taskid} AND USERGROUP_ID = ${usergroupid};`
+      );
     res.json(user.recordset);
   }
 
@@ -64,8 +76,8 @@ class TasksController {
       TASK_GROUP,
       TASK_DESCRIPTION,
       NOTE,
-      taskid,
-      usergroup,
+      ID,
+      usergroupid,
     } = req.query;
     const pool = await sql.connect(config);
     const users = await pool.request().query(
@@ -79,45 +91,46 @@ class TasksController {
       [LAST_CHANGE] = GETDATE(),
       [NOTE] = '${NOTE}'
       OUTPUT inserted.*
-      WHERE USERGROUP_ID = ${usergroup} AND ID = ${taskid};`
+      WHERE USERGROUP_ID = ${usergroupid} AND ID = ${ID};`
     );
     res.json(users.recordset);
   }
   async deleteTask(req, res) {
-    const { taskid, usergroup } = req.params;
+    const { taskid, usergroupid } = req.params;
     const pool = await sql.connect(config);
     const users = await pool.request().query(
       `DELETE FROM [TASKS] 
       OUTPUT deleted.*
-      WHERE USERGROUP_ID = ${usergroup} AND ID = '${taskid}';`
+      WHERE USERGROUP_ID = ${usergroupid} AND ID = '${taskid}';`
     );
     res.json(users.recordset);
   }
 
   async getTasksLastChange(req, res) {
-    const { lastchange, usergroup } = req.query;
+    const { lastchange, USERGROUP_ID } = req.query;
     const pool = await sql.connect(config);
     const tasks = await pool.request().query(
-      `SELECT * FROM TASKS WHERE USERGROUP_ID = ${usergroup} 
+      `SELECT * FROM TASKS WHERE USERGROUP_ID = ${USERGROUP_ID} 
         AND LAST_CHANGE >= '${lastchange}'
         AND LAST_CHANGE IS NOT NULL
         ;`
     );
+
     res.json(tasks.recordset);
   }
 
-  async updateTaskStatus(req, res) {
-    const { status, usergroup, taskid, userid } = req.query;
+  async updateTaskStatus2(req, res) {
+    const { status, USERGROUP_ID, ID, userid } = req.query;
 
     let queryType = '';
     switch (status) {
-      case 'inprocess':
+      case 'start':
         //START
         queryType = `
         [STARTED] = GETDATE(),
         `;
         break;
-      case 'done':
+      case 'finish':
         //FINISH
         queryType = `
         [FINISHED] = GETDATE(),
@@ -141,9 +154,110 @@ class TasksController {
       [USERID] = '${userid}',
       [LAST_CHANGE] = GETDATE()
       OUTPUT inserted.*
-      WHERE USERGROUP_ID = ${usergroup} AND ID = ${taskid};`
+      WHERE USERGROUP_ID = ${USERGROUP_ID} AND ID = ${ID};`
     );
     res.json(users.recordset);
+  }
+
+  async updateTaskStatus(req, res) {
+    const { status, USERGROUP_ID, ID, userid } = req.query;
+
+    let queryType = '';
+
+    switch (status) {
+      case 'start':
+        //START
+        queryType = `
+        [STARTED] = GETDATE(),
+        `;
+        break;
+      case 'finish':
+        //FINISH
+        queryType = `
+        [FINISHED] = GETDATE(),
+        [AVERAGE_TIME] = (IIF([AVERAGE_TIME] IS NOT NULL,[AVERAGE_TIME],1) * 0.8) 
+        + ( DATEDIFF(mi,[STARTED],GETDATE()) * 0.2),
+        `;
+        break;
+      default:
+        //RESET
+        queryType = `
+        [STARTED] = NULL,
+        [FINISHED] = NULL,
+        `;
+        break;
+    }
+    const pool = await sql.connect(config);
+    const updStatus = await pool.request().query(
+      `UPDATE [TASKS] 
+      SET 
+      ${queryType}
+      [USERID] = '${userid}',
+      [LAST_CHANGE] = GETDATE()
+      OUTPUT inserted.*
+      WHERE USERGROUP_ID = ${USERGROUP_ID} AND ID = ${ID};`
+    );
+
+    let id = updStatus.recordset[0].ID;
+    if (id) {
+      let setFinished =
+        status === 'finish'
+          ? `'${getDateFormat(updStatus.recordset[0].FINISHED)}'`
+          : 'NULL';
+      let setStarted =
+        status !== 'reset'
+          ? `'${getDateFormat(updStatus.recordset[0].STARTED)}'`
+          : 'NULL';
+      let archiveQuery =
+        status !== 'reset'
+          ? `IF EXISTS (
+        SELECT * FROM TASK_ARCHIVE
+        WHERE 
+        USERGROUP_ID = ${updStatus.recordset[0].USERGROUP_ID} 
+        AND TASK_ID = ${id}
+        AND DATE_ = CAST(GETDATE() AS DATE)
+      )
+        BEGIN
+          UPDATE TASK_ARCHIVE
+          SET 
+          STARTED = ${setStarted} ,
+          FINISHED = ${setFinished} ,
+          USERID = '${updStatus.recordset[0].USERID}'
+          WHERE
+          USERGROUP_ID = ${updStatus.recordset[0].USERGROUP_ID} 
+          AND TASK_ID = ${id}
+          AND DATE_ = CAST(GETDATE() AS DATE);
+        END
+      ELSE
+        BEGIN
+        INSERT INTO TASK_ARCHIVE
+        ([TASK_ID]
+          ,[TASK_NAME]
+          ,[STARTED]
+          ,[FINISHED]
+          ,[USERID]
+          ,[USERGROUP_ID]
+          ,[DATE_])
+        VALUES
+        (
+          ${updStatus.recordset[0].ID}
+          ,N'${updStatus.recordset[0].TASK_NAME}'
+          ,${setStarted}
+          ,${setFinished}
+          ,N'${updStatus.recordset[0].USERID}'
+          ,${updStatus.recordset[0].USERGROUP_ID}
+          ,CAST(GETDATE() AS DATE)
+        )
+        END`
+          : `DELETE FROM TASK_ARCHIVE
+      WHERE
+      USERGROUP_ID = ${USERGROUP_ID} 
+      AND TASK_ID = ${ID}
+      AND DATE_ = CAST(GETDATE() AS DATE)
+      `;
+      const archive = await pool.request().query(archiveQuery);
+    }
+    res.json(updStatus.recordset);
   }
 }
 
--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -4,14 +4,14 @@ const TasksController = require('../controllers/tasks.controller');
 
 router.post('/tasks', TasksController.createTask);
 
-router.get('/tasks/list/:usergroup', TasksController.getTasks);
-router.get('/tasks/list/:usergroup/:taskid', TasksController.getOneTask);
-router.get('/tasks/totalcount/:usergroup', TasksController.countTasks);
+router.get('/tasks/list/:usergroupid', TasksController.getTasks);
+router.get('/tasks/list/:usergroupid/:taskid', TasksController.getOneTask);
+router.get('/tasks/totalcount/:usergroupid', TasksController.countTasks);
 router.get('/tasks/changes', TasksController.getTasksLastChange);
 
-router.put('/tasks', TasksController.updateTask);
+router.put('/tasks/task', TasksController.updateTask);
 router.put('/tasks/task_status/', TasksController.updateTaskStatus);
 
-router.delete('/tasks/:usergroup/:taskid', TasksController.deleteTask);
+router.delete('/tasks/:usergroupid/:taskid', TasksController.deleteTask);
 
 module.exports = router;
