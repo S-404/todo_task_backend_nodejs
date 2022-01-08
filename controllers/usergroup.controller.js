dea6b4b3857b20ec4a7328023981b0ec9d273c09
Andrey <sudarev.andrey.i@gmail.com>
2022-01-08 18:03:51 +0300

    upd usergroup

--- controllers/usergroup.controller.js
+++ controllers/usergroup.controller.js
@@ -3,13 +3,24 @@ const config = require('../config');
 
 class UserGroupController {
   async createUsergroup(req, res) {
-    const { name } = req.query;
+    const { name, userid } = req.query;
     const pool = await sql.connect(config);
     const newUG = await pool.request().query(
       `INSERT INTO [USERGROUPS] (USERGROUP) 
         OUTPUT inserted.*
         VALUES ('${name}');`
     );
+    let usergroup = newUG.recordset[0].USERGROUP_ID;
+    if (usergroup) {
+      const newUGAdmin = await pool.request().query(
+        `INSERT INTO USERS_ACCESS 
+        (USERGROUP_ID, USERID, ISADMIN) 
+        OUTPUT inserted.*
+        VALUES 
+        (${usergroup},'${userid}',1);`
+      );
+    }
+
     res.json(newUG.recordset);
   }
 
@@ -19,7 +30,7 @@ class UserGroupController {
     const userList = await pool.request().query(
       `SELECT utable.USERGROUP_ID, utable.USERID, utable.ISADMIN, ugtable.USERGROUP
       FROM USERGROUPS as ugtable 
-      RIGHT OUTER JOIN USERS as utable  
+      RIGHT OUTER JOIN USERS_ACCESS as utable  
       ON ugtable.USERGROUP_ID = utable.USERGROUP_ID  
       WHERE utable.USERID = '${userid}'
       ;`
